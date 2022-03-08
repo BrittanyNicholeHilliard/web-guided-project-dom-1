bb32014c9aa328fee115ad836077ff167b799d73
Brittany Hilliard <bnicholehilliard@gmail.com>
2022-03-08 14:47:32 -0800

    update

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const classElems = document.getElementsByClassName("menu-item");
 //console.log(classElems) // => DOM node list
 
 //querySelector -- returns first element it finds
-const funElem= document.querySelector(". card") //==> Targets anything classed as card in css file
+const funElem= document.querySelector(".card") //==> Targets anything classed as card in css file
 const navElem = document.querySelector("#mainNav")
 
 //querySelectorAll -returns node list of all elements that match
@@ -24,15 +24,15 @@ const menuElems =document.querySelectorAll("menu-item");
 
 // A- finding across the entire DOM
 const header = document.querySelector('header');
-const logoTitle = document.querySelector('#logoTitle');
-const firstCard = document.querySelector(".card:nth-of-type('1')");
+const logoTitle = document.querySelector("#logoTitle");
+const firstCard = document.querySelector(".card:nth-of-type(1)");
 // B- finding within one particular element
 const imageFirstCard = firstCard.querySelector("img");
 const titleFirstCard = firstCard.querySelector("h2");
 const subtitleFirstCard = firstCard.querySelector("h3");
 const textFirstCard = firstCard.querySelector("p");
 // C- traversing with dot notation
-const link1FirstCard = firstCard.nextElementSibling;
+const link1FirstCard = textFirstCard.nextElementSibling;
 const link2FirstCard = link1FirstCard.nextElementSibling;
 
 
@@ -56,13 +56,18 @@ const homeElem = linksRealArray.find(link => {
 console.log(homeElem);
 // 👉 3- Changing an element's text content
 //  A- Change the cat-related content into dog-related content
+logoTitle.textContent = "BloomTech Dogz";
+titleFirstCard.textContent = "Dogz RULE!";
+
 //  B- Have the students research online the difference between textContent and innerText
 
 
 // 👉 4- Changing any property
 //  A- Using dot notation to change a few attributes
-//  B- Using .setAttribute to change a few attributes
+header.querySelector("img").src = "https://upload.wikimedia.org/wikipedia/commons/c/c5/M%C3%A5rten_Eskil_Winge_-_Tor%27s_Fight_with_the_Giants_-_Google_Art_Project.jpg"
 
+//  B- Using .setAttribute to change a few attributes
+imageFirstCard.src = "https://static.wikia.nocookie.net/dog/images/7/7e/Americanstaffordterrier.jpeg/revision/latest/top-crop/width/360/height/450?cb=20201129045715"
 
 // 👉 5- Changing the styling of an element
 //  A- By changing the class names on the element with the classList API
